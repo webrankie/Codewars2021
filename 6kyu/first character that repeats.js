d862dcb04393969f100fb948b9da93958184229d
engineer <sandrey.kz@gmail.com>
2021-04-13 16:12:15 -0700

    Kata: first character that repeats

---
+++ 6kyu/first character that repeats.js
@@ -0,0 +1,13 @@
+/*
+Find the first character that repeats in a String and return that character.
+
+
+firstDup('tweet') => 't'
+firstDup('like') => undefined
+
+ */
+
+function firstDup (s) {
+    s = s.split('');
+    return s.filter((el) => s.indexOf(el) !== s.lastIndexOf(el))[0];
+}
